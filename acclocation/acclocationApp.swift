c0383caf40ed7fdafc478a0c6e05d85989420ed6
Nao-923 <fnet.non@outlook.jp>
2024-12-23 16:12:53 +0900

    Initial Commit

---
+++ acclocation/acclocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  acclocationApp.swift
+//  acclocation
+//
+//  Created by fnet on 2024/12/23.
+//
+
+import SwiftUI
+
+@main
+struct acclocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ acclocationTests/acclocationTests.swift
@@ -0,0 +1,17 @@
+//
+//  acclocationTests.swift
+//  acclocationTests
+//
+//  Created by fnet on 2024/12/23.
+//
+
+import Testing
+@testable import acclocation
+
+struct acclocationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
